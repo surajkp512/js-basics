577fddf69c9db63ed68aef6f1312795229d71416
Suraj Kumar Parida <42241876+surajkp512@users.noreply.github.com>
2023-11-14 16:07:21 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -15,3 +15,34 @@ console.log(booleanFlag);
 // ""=> false
 //"mandy"=> true 
 
+//OPERATIONS
+
+let string1 = "suraj"
+let string2 = " kumar"
+let string3 = " parida"
+
+let str4 = string1 +string2 +string3
+console.log(str4); 
+
+// console.log(1+"2");
+// console.log("1"+2+1);
+// console.log(1+"2"+1);
+// console.log("1"+2+2);
+
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(x,y);
+// Expected output: "x:4, y:3",
+
+let a = 3;
+const b = ++a;
+ 
+console.log(a,b);
+console.log(a+b);
+// Expected output: "a:4, b:4"
+
