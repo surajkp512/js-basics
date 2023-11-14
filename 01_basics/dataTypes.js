6c010a31740e32bbceb275c1584c45ebcf7cfd8c
Suraj Kumar Parida <42241876+surajkp512@users.noreply.github.com>
2023-11-14 15:34:07 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; 
+
+//alert("hello") we are using node JS not browser
+console.log("suraj");
+let name = "suraj"
+let age = 24
+let isLoggedIn = false
+let state;
+
+console.log(typeof null)
---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountID = 1445
+{
+let accountEmail = "suraj@gmail.com"
+var accountPassword = "123456"
+accountCity = "Delhi"
+}
+//accountID =2 not allowed 
+{
+accountEmail = "yash@gmail.com"
+accountPassword = "122334"
+accountCity = "jaipur"
+}
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
+
