258f37a7f837f83db54a93ffe56e04adb22e70ac
Suraj Kumar Parida <42241876+surajkp512@users.noreply.github.com>
2023-11-14 15:45:11 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,17 @@
+let score = 22
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInScore = Number(score)
+console.log(typeof valueInScore);
+console.log(valueInScore); 
+
+let flag ="mandy"
+let  booleanFlag = Boolean(flag)
+console.log(booleanFlag);
+
+// 1=> true 0 => false
+// ""=> false
+//"mandy"=> true 
+
