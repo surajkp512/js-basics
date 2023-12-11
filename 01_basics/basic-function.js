40efcb39eeee2d4674f4fece0ad31d235b5ff416
Suraj Kumar Parida <42241876+surajkp512@users.noreply.github.com>
2023-12-11 12:43:03 +0000

    update

---
+++ 01_basics/basic-function.js
@@ -0,0 +1,17 @@
+
+
+const names= ["gojo", "naruto", "eren", "mikasa"];
+let obj1=  {
+    name: "suraj",
+    age: 24,
+    sex: "male"
+}
+
+
+const myFunction= function(){
+ console.log("Hello World")
+}
+
+console.log(typeof names);
+
+
