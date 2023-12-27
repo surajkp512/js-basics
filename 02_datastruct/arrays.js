40278ed38328f61ef12921440b5f4c658f835bee
Suraj Kumar Parida <surajkp512@gmail.com>
2023-12-27 21:11:17 +0530

    git try

--- 02_datastruct/arrays.js
+++ 02_datastruct/arrays.js
@@ -5,8 +5,18 @@ const myHeroes = ["iron man", "black widow", "hulk"]
 console.log(myArr[3]);
 
 
-myArr.push(5)
+myArr.push(6)
 console.log(myArr)
 
-myArr.pop()
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(4))
+
+
+
+
+const popped = myArr.pop()
+console.log(myArr)
+console.log(`Popped element is ${popped} `)
+
+myArr.shift()
 console.log(myArr)
