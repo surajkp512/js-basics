f6a8a834392652df95f30016c444b3dad1b1e45c
Suraj Kumar Parida <42241876+surajkp512@users.noreply.github.com>
2023-12-27 15:20:42 +0000

    trying git

---
+++ 02_datastruct/arrays.js
@@ -0,0 +1,12 @@
+//array 
+
+const myArr = [0,1,2,3,4,6]
+const myHeroes = ["iron man", "black widow", "hulk"]
+console.log(myArr[3]);
+
+
+myArr.push(5)
+console.log(myArr)
+
+myArr.pop()
+console.log(myArr)
